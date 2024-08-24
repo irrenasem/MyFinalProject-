b5a47de7530a71f655f19b01499369b68b8c14f7
irrenasem <137314150+irrenasem@users.noreply.github.com>
2024-08-24 14:26:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import os
 from datetime import datetime
 
 BASE_URL = 'http://api.weatherapi.com'
-def get_weather(api_key, city):
+def get_weather(api_key, city_name):
+    # Construct the URL for current weather
     url = f"{BASE_URL}/v1/current.json?q={city}&key={api_key}"
     response = requests.get(url)
     data = response.json()
     if response.status_code == 200:
         temperature = data['current']['temp_c']
         condition = data['current']['condition']['text']
-        return f"Current temperature in {city} is {temperature}°C, Condition: {condition}"
+        return f"Current temperature in {city_name} is {temperature}°C, Condition: {condition}"
     else:
         return "Failed to fetch weather data"
 
 api_key = os.getenv('API_TOKEN')
-city = 'Tel Aviv'
-print(get_weather(api_key, city))
+city_name = 'Tel Aviv'
+print(get_weather(api_key, city_name))
